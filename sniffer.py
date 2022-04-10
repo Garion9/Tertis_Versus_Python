ae6c570f0333b20c5a7a125b89c1d140a4e45cb2
Garion9 <mpalczak2@gmail.com>
2022-04-10 09:02:52 +0000

    Changed sniffer to work on Linux (Ubuntu)

--- sniffer.py
+++ sniffer.py
@@ -4,8 +4,8 @@ import binascii
 
 HOST = socket.gethostbyname(socket.gethostname())
 
-s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_IP)
-s.bind((HOST, 10000))
+s = socket.socket(socket.PF_PACKET, socket.SOCK_RAW, socket.ntohs(0x0003))
+# s.bind((HOST, 10000))
 # s.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
 # s.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
 
@@ -16,8 +16,9 @@ while True:
 
     eth_header = struct.unpack("!6s6s2s", ethernet_header)
 
-    print("Destination MAC:" + str(binascii.hexlify(eth_header[0], sep=":", bytes_per_sep=1)) + " Source MAC:" + str(binascii.hexlify(eth_header[1], sep=":", bytes_per_sep=1)) + " Type:" + str(binascii.hexlify(eth_header[2])))
+    print("Destination MAC:" + str(binascii.hexlify(eth_header[0], sep=":", bytes_per_sep=1)) + "  | Source MAC:" + str(binascii.hexlify(eth_header[1], sep=":", bytes_per_sep=1)) + " Type:" + str(binascii.hexlify(eth_header[2])))
 
     ipheader = packet[0][14:34]
     ip_header = struct.unpack("!12s4s4s", ipheader)
-    print("Source IP:" + socket.inet_ntoa(ip_header[1]) + " Destination IP:" + socket.inet_ntoa(ip_header[2]))
+    print("Destination IP:" + socket.inet_ntoa(ip_header[2]) + " \t\t  | Source IP:" + socket.inet_ntoa(ip_header[1]))
+    print("------------------------------------------------------------------------------------")
