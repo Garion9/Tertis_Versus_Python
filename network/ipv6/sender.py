88d7c8ea58a3d37b6f5e6d0fef44092538cf4943
Dewkong <kryniu113@gmail.com>
2022-05-20 12:22:10 +0200

    Transformed sender.py into a class but it still has main (need to modify Tertis to use this version)

--- network/ipv6/sender.py
+++ network/ipv6/sender.py
@@ -5,43 +5,38 @@ import binascii
 from network.ipv6.util import standardize_ipv6_format, calculate_checksum
 
 
-def main():
-    source_port = 50000
-    destination_port = 50001
+class Sender:
+    def __init__(self, interface_name, mac_address_remote, ip_address_remote):
+        self.__source_port = 50000
+        self.__destination_port = 50001
 
-    print(netifaces.interfaces())
+        self.__interface_name = interface_name
+        mac_address_local = netifaces.ifaddresses(self.__interface_name)[netifaces.AF_PACKET][0]['addr']
+        ip_address_local = netifaces.ifaddresses(self.__interface_name)[netifaces.AF_INET6][0]['addr']
+        ip_address_local = standardize_ipv6_format(ip_address_local)
+        ip_address_remote = standardize_ipv6_format(ip_address_remote)
 
-    interface_name = input("Enter network interface name: ")
-    mac_address_local = netifaces.ifaddresses(interface_name)[netifaces.AF_PACKET][0]['addr']
-    ip_address_local = netifaces.ifaddresses(interface_name)[netifaces.AF_INET6][0]['addr']
-    ip_address_local = standardize_ipv6_format(ip_address_local)
-    mac_address_remote = input("Enter remote MAC address: ")
-    ip_address_remote = input("Enter remote IPv6 address: ")
-    ip_address_remote = standardize_ipv6_format(ip_address_remote)
+        self.__mac_address_local_bytes = binascii.unhexlify(mac_address_local.replace(':', ''))
+        self.__ip_address_local_bytes = socket.inet_pton(socket.AF_INET6, ip_address_local)
+        self.__mac_address_remote_bytes = binascii.unhexlify(mac_address_remote.replace(':', ''))
+        self.__ip_address_remote_bytes = socket.inet_pton(socket.AF_INET6, ip_address_remote)
 
-    mac_address_local_bytes = binascii.unhexlify(mac_address_local.replace(':', ''))
-    ip_address_local_bytes = socket.inet_pton(socket.AF_INET6, ip_address_local)
-    mac_address_remote_bytes = binascii.unhexlify(mac_address_remote.replace(':', ''))
-    ip_address_remote_bytes = socket.inet_pton(socket.AF_INET6, ip_address_remote)
+        self.__s = socket.socket(socket.AF_PACKET, socket.SOCK_RAW)
+        self.__s.bind((self.__interface_name, 0))
 
-    s = socket.socket(socket.AF_PACKET, socket.SOCK_RAW)
-    s.bind((interface_name, 0))
-
-    while True:
-        data = bytes(input("Enter message: "), 'utf-8')
-
+    def create_packet(self, data):
         payload_length = len(data) + 8
         payload_length_hex = hex(payload_length).lstrip("0x").rstrip("L")
         payload_length_hex_padded = "0" * (4 - len(payload_length_hex)) + payload_length_hex
         payload_length_bytes = binascii.unhexlify(payload_length_hex_padded)
 
-        source_port_hex = hex(source_port).lstrip("0x").rstrip("L")
+        source_port_hex = hex(self.__source_port).lstrip("0x").rstrip("L")
         source_port_bytes = binascii.unhexlify("0" * (4 - len(source_port_hex)) + source_port_hex)
-        destination_port_hex = hex(destination_port).lstrip("0x").rstrip("L")
+        destination_port_hex = hex(self.__destination_port).lstrip("0x").rstrip("L")
         destination_port_bytes = binascii.unhexlify("0" * (4 - len(destination_port_hex)) + destination_port_hex)
 
-        ip_pseudo_header = ip_address_local_bytes
-        ip_pseudo_header += ip_address_remote_bytes
+        ip_pseudo_header = self.__ip_address_local_bytes
+        ip_pseudo_header += self.__ip_address_remote_bytes
         ip_pseudo_header += binascii.unhexlify("0" * (32 - len(payload_length_hex)) + payload_length_hex)
         ip_pseudo_header += b'\x00\x00\x00\x11'
         ip_pseudo_header += source_port_bytes
@@ -62,17 +57,34 @@ def main():
         ip_header = b'\x60\x00\x00\x00'  # Version, Traffic Class, Flow Label
         ip_header += payload_length_bytes  # Payload Length
         ip_header += b'\x11\x40'  # Next Header, Hop Limit
-        ip_header += ip_address_local_bytes  # Source Address
-        ip_header += ip_address_remote_bytes  # Destination Address
+        ip_header += self.__ip_address_local_bytes  # Source Address
+        ip_header += self.__ip_address_remote_bytes  # Destination Address
 
-        ethernet = mac_address_remote_bytes  # MAC Address Destination
-        ethernet += mac_address_local_bytes  # MAC Address Source
+        ethernet = self.__mac_address_remote_bytes  # MAC Address Destination
+        ethernet += self.__mac_address_local_bytes  # MAC Address Source
         ethernet += b'\x86\xdd'  # Protocol-Type: IPv6
 
         crc = binascii.crc32(ethernet + ip_header + udp + data).to_bytes(4, 'little')
 
-        packet = ethernet + ip_header + udp + data + crc
-        s.send(packet)
+        return ethernet + ip_header + udp + data + crc
+
+    def send(self, data):
+        data = bytes(data, 'utf-8')
+        packet = self.create_packet(data)
+        self.__s.send(packet)
+
+
+def main():
+    print(netifaces.interfaces())
+    interface_name = input("Enter network interface name: ")
+    mac_address_remote = input("Enter remote MAC address: ")
+    ip_address_remote = input("Enter remote IPv6 address: ")
+
+    sender = Sender(interface_name, mac_address_remote, ip_address_remote)
+
+    while True:
+        data = input("Enter message: ")
+        sender.send(data)
 
 
 if __name__ == "__main__":
