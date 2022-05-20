dfd093f335cdd9732ac24a3bf3c7f41375b38be7
Garion <mpalczak2@gmail.com>
2022-05-20 21:08:28 +0200

    Refactored Tertis techdemo (pygame_testing) to use new versions of receiver and sender

--- tertis/pygame_testing.py
+++ tertis/pygame_testing.py
@@ -1,108 +1,113 @@
 import pygame
-
-import socket
-import struct
-import netifaces
-import binascii
-
-from network.ipv6.util import standardize_ipv6_format
-
 import threading
+
+from typing import Tuple, Union
+from pygame.surface import Surface, SurfaceType
+
+from network.ipv6.sender import Sender
+from network.ipv6.receiver import Receiver
+
+
+class Colours:
+    Red = (255, 0, 0)
+    OrangeRed = (255, 69, 0)
+    Green = (0, 255, 0)
+    Blue = (0, 0, 255)
+    LightBlue = (0, 191, 255)
+    White = (255, 255, 255)
+    Black = (0, 0, 0)
+
+
+class GameBoard:
+    def __init__(self, background_color: Tuple[int, int, int], border_color: Tuple[int, int, int]):
+        self.x_lower_bound = 0
+        self.x_upper_bound = 400
+        self.y_lower_bound = 0
+        self.y_upper_bound = 600
+        self.block_side_length = 40
+        self.unit_of_movement = 40
+        self.starting_position_x = 200
+        self.starting_position_y = 200
+        self.fps = 60
+        self.background_color = background_color
+        self.border_color = border_color
+
+    def draw(self, screen: Union[Surface, SurfaceType]):
+        screen.fill(self.background_color)
+        pygame.draw.line(screen, Colours.Black, (self.x_upper_bound, self.y_lower_bound), (self.x_upper_bound, self.y_upper_bound), 1)
+
+
+class Player:
+    class PlayerPosition:
+        def __init__(self, x: int, y: int):
+            self.x = x
+            self.y = y
+
+    class PlayerSpeed:
+        def __init__(self, horizontal: int, vertical: int):
+            self.horizontal = horizontal
+            self.vertical = vertical
+
+    def __init__(self, starting_position_x: int, starting_position_y: int, color: Tuple[int, int, int], is_controlled: bool = True):
+        self.position = self.PlayerPosition(starting_position_x, starting_position_y)
+        self.speed = self.PlayerSpeed(0, 0) if is_controlled else None
+        self.color = color
+
+    def move(self, game_board: GameBoard):
+        if self.speed.horizontal != 0:
+            self.position.x += self.speed.horizontal
+            self.position.x = max(min(self.position.x, game_board.x_upper_bound - game_board.block_side_length), game_board.x_lower_bound)
+            return True
+        if self.speed.vertical != 0:
+            self.position.y += self.speed.vertical
+            self.position.y = max(min(self.position.y, game_board.y_upper_bound - game_board.block_side_length), game_board.y_lower_bound)
+            return True
+        return False
+
+    def draw(self, screen: Union[Surface, SurfaceType], game_board: GameBoard):
+        pygame.draw.rect(screen, self.color, pygame.Rect(self.position.x, self.position.y, game_board.block_side_length, game_board.block_side_length))
 
 
 class ReceiverThread(threading.Thread):
-    def __init__(self, interface_name, mac_address_remote, ip_address_remote, remote_positions):
+    def __init__(self, interface_name: str, mac_address_remote: str, ip_address_remote: str, opponent_position: Player.PlayerPosition, game_board: GameBoard):
         super().__init__()
-        self.interface_name = interface_name
-        self.mac_address_local = netifaces.ifaddresses(self.interface_name)[netifaces.AF_PACKET][0]['addr']
-        self.ip_address_local = netifaces.ifaddresses(self.interface_name)[netifaces.AF_INET6][0]['addr']
-        self.ip_address_local = standardize_ipv6_format(self.ip_address_local)
-        self.mac_address_remote = mac_address_remote
-        self.ip_address_remote = ip_address_remote
-        self.ip_address_remote = standardize_ipv6_format(self.ip_address_remote)
-        self.remote_positions = remote_positions
+
+        self.opponent_position = opponent_position
+
+        def update_opponent_positions(packet):
+            payload = str(packet[62:-4], 'utf-8')
+            position_updates = payload.split(":")
+            self.opponent_position.x = int(position_updates[1]) + game_board.x_upper_bound
+            self.opponent_position.y = int(position_updates[3])
+
+        self.network_receiver = Receiver(interface_name, mac_address_remote, ip_address_remote, update_opponent_positions)
 
     def run(self):
-        s = socket.socket(socket.PF_PACKET, socket.SOCK_RAW, socket.ntohs(0x0003))
-        while True:
-            packet = s.recvfrom(2048)
-            ethernet_header = packet[0][0:14]
-            eth_header = struct.unpack("!6s6s2s", ethernet_header)
-            ipheader = packet[0][14:54]
+        self.network_receiver.receive()
 
-            if str(binascii.hexlify(eth_header[0], sep=":", bytes_per_sep=1), "utf-8") == str(self.mac_address_local) and \
-                    str(binascii.hexlify(eth_header[1], sep=":", bytes_per_sep=1), "utf-8") == str(
-                self.mac_address_remote) and \
-                    standardize_ipv6_format(socket.inet_ntop(socket.AF_INET6, ipheader[24:40])) == self.ip_address_local and \
-                    standardize_ipv6_format(socket.inet_ntop(socket.AF_INET6, ipheader[8:24])) == self.ip_address_remote:
-
-                updated_coordinates = str(packet[0][54:], 'utf-8').split(":")
-
-                self.remote_positions["x"], self.remote_positions["y"] = int(updated_coordinates[1]), int(updated_coordinates[3])
-
-
-
-    def get_positions(self):
-        return self.remote_positions
 
 def main():
     interface_name = input("Enter network interface name: ")
-    mac_address_local = netifaces.ifaddresses(interface_name)[netifaces.AF_PACKET][0]['addr']
-    ip_address_local = netifaces.ifaddresses(interface_name)[netifaces.AF_INET6][0]['addr']
-    ip_address_local = standardize_ipv6_format(ip_address_local)
     mac_address_remote = input("Enter remote MAC address: ")
     ip_address_remote = input("Enter remote IPv6 address: ")
-    ip_address_remote = standardize_ipv6_format(ip_address_remote)
-
-    mac_address_local_bytes = binascii.unhexlify(mac_address_local.replace(':', ''))
-    ip_address_local_bytes = socket.inet_pton(socket.AF_INET6, ip_address_local)
-    mac_address_remote_bytes = binascii.unhexlify(mac_address_remote.replace(':', ''))
-    ip_address_remote_bytes = socket.inet_pton(socket.AF_INET6, ip_address_remote)
-
-    s = socket.socket(socket.AF_PACKET, socket.SOCK_RAW)
-    s.bind((interface_name, 0))
-
-    ethernet = mac_address_remote_bytes  # MAC Address Destination
-    ethernet += mac_address_local_bytes  # MAC Address Source
-    ethernet += b'\x86\xdd'  # Protocol-Type: IPv6
-
-    ip_header = b'\x60\x00\x00\x00'  # Version, Traffic Class, Flow Label
-    ip_header += b'\x04\x00\x06\x40'  # Payload Length, Next Header, Hop Limit
-    ip_header += ip_address_local_bytes  # Source Address
-    ip_header += ip_address_remote_bytes  # Destination Address
-
-    # pygame starts here
+    network_sender = Sender(interface_name, mac_address_remote, ip_address_remote)
 
     pygame.init()
 
-    x_lower_bound = 0
-    x_upper_bound = 400
-    y_lower_bound = 0
-    y_upper_bound = 600
+    game_board = GameBoard(Colours.White, Colours.Black)
+    player = Player(game_board.starting_position_x, game_board.starting_position_y, Colours.LightBlue)
+    opponent = Player(game_board.starting_position_x + game_board.x_upper_bound, game_board.starting_position_y, Colours.OrangeRed, is_controlled=False)
 
-    x_pos = 200
-    y_pos = 200
-    side_length = 40
-    unit_of_movement = side_length
-    x_pos_mod = 0
-    y_pos_mod = 0
-
-    remote_x_pos = 200
-    remote_y_pos = 200
-    remote_positions = {"x": remote_x_pos, "y": remote_y_pos}
-
-    fps = 60
-    time = 0
-
-    screen = pygame.display.set_mode([2*x_upper_bound, y_upper_bound])
+    screen = pygame.display.set_mode([2*game_board.x_upper_bound, game_board.y_upper_bound])
     pygame.display.set_caption("TERTIS Versus (WIP)")
 
     running = True
 
+    time = 0
     clock = pygame.time.Clock()
 
-    receiverThread = ReceiverThread(interface_name, mac_address_remote, ip_address_remote, remote_positions)
-    receiverThread.start()
+    receiver_thread = ReceiverThread(interface_name, mac_address_remote, ip_address_remote, opponent.position, game_board)
+    receiver_thread.start()
 
     while running:
         for event in pygame.event.get():
@@ -112,54 +117,42 @@ def main():
                 if event.key == pygame.K_ESCAPE:
                     running = False
                 elif event.key == pygame.K_UP:
-                    y_pos_mod -= unit_of_movement
+                    player.speed.vertical -= game_board.unit_of_movement
                 elif event.key == pygame.K_DOWN:
-                    y_pos_mod += unit_of_movement
+                    player.speed.vertical += game_board.unit_of_movement
                 elif event.key == pygame.K_LEFT:
-                    x_pos_mod -= unit_of_movement
+                    player.speed.horizontal -= game_board.unit_of_movement
                 elif event.key == pygame.K_RIGHT:
-                    x_pos_mod += unit_of_movement
+                    player.speed.horizontal += game_board.unit_of_movement
             elif event.type == pygame.KEYUP:
                 if event.key == pygame.K_UP:
-                    y_pos_mod += unit_of_movement
+                    player.speed.vertical += game_board.unit_of_movement
                     time = 251
                 elif event.key == pygame.K_DOWN:
-                    y_pos_mod -= unit_of_movement
+                    player.speed.vertical -= game_board.unit_of_movement
                     time = 251
                 elif event.key == pygame.K_LEFT:
-                    x_pos_mod += unit_of_movement
+                    player.speed.horizontal += game_board.unit_of_movement
                     time = 251
                 elif event.key == pygame.K_RIGHT:
-                    x_pos_mod -= unit_of_movement
+                    player.speed.horizontal -= game_board.unit_of_movement
                     time = 251
 
-        screen.fill((255, 255, 255))
+        game_board.draw(screen)
 
         if time > 200:
-            if x_pos_mod != 0:
-                x_pos += x_pos_mod
-                x_pos = max(min(x_pos, x_upper_bound - side_length), x_lower_bound)
-                time = 0
-            if y_pos_mod != 0:
-                y_pos += y_pos_mod
-                y_pos = max(min(y_pos, y_upper_bound - side_length), y_lower_bound)
+            if player.move(game_board):
                 time = 0
 
-        pygame.draw.rect(screen, (255, 0, 0), pygame.Rect(x_pos, y_pos, side_length, side_length))
-
-        remote_positions = receiverThread.get_positions()
-
-        pygame.draw.rect(screen, (0, 0, 255), pygame.Rect(remote_positions.get("x") + x_upper_bound, remote_positions.get("y") + y_lower_bound, side_length, side_length))
-
-        pygame.draw.line(screen, (0, 0, 0), (x_upper_bound, y_lower_bound), (x_upper_bound, y_upper_bound), 1)
+        player.draw(screen, game_board)
+        opponent.draw(screen, game_board)
 
         pygame.display.flip()
 
-        payload = bytes("x:"+str(x_pos)+":y:"+str(y_pos), 'utf-8')
-        packet = ethernet + ip_header + payload
-        s.send(packet)
+        message = "x:"+str(player.position.x)+":y:"+str(player.position.y)
+        network_sender.send(message)
 
-        time += clock.tick(fps)
+        time += clock.tick(game_board.fps)
 
     pygame.quit()
 
